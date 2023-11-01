4baa0856f3d8da87bc2874f220269f405a58be83
Martins <oyedelenewton@gmail.com>
2023-11-01 16:30:48 -0700

    Task Zero And One

--- shell.c
+++ shell.c
@@ -1,10 +1,12 @@
 #include "shell.h"
 
-/* main - Entry point to simple shell project
-* @ac: number of arguments parsed
-* @av: an arry of arguments parsed
-* @en: an array of environmental variables
-*/
+/**
+ * main - Entry point to simple shell project
+ * @ac: number of arguments parsed
+ * @av: an arry of arguments parsed
+ * @en: an array of environmental variables
+ * Return: 0
+ */
 int main(int ac, char **av, char *en[])
 {
 	char *cmd = NULL;
@@ -12,14 +14,13 @@ int main(int ac, char **av, char *en[])
 	char *args[2];
 	pid_t child_pid;
 	int pid_status;
-	
+
 	(void)ac;
 	(void)av;
 	(void)en;
-
 	while (1)
 	{
-		printf("OurShell $ " );
+		printf("OurShell $ ");
 		if (getline(&cmd, &cmdsize, stdin) == -1)
 		{
 			printf("\n");
@@ -29,13 +30,11 @@ int main(int ac, char **av, char *en[])
 		cmd[strcspn(cmd, "\n")] = '\0';
 		args[0] = cmd;
 		args[1] = NULL;
-		
 		child_pid = fork();
 		if (child_pid == -1)
 		{
 			printf("%s: No such file or directory\n", av[0]);
 		}
-
 		if (child_pid == 0)
 		{
 			if (execve(cmd, args, NULL) == -1)
@@ -47,7 +46,6 @@ int main(int ac, char **av, char *en[])
 		{
 			waitpid(child_pid, &pid_status, 0);
 		}
-
 	}
 	free(cmd);
 	return (0);
