5f8fad019fa67758a9e63d228bd1ca389b90f9aa
Martins <oyedelenewton@gmail.com>
2023-11-01 15:45:24 -0700

    So far

--- shell.c
+++ shell.c
@@ -1,20 +1,35 @@
 #include "shell.h"
 
-int main(void)
+int main(int ac, char **av, char *en[])
 {
-	char *cmd;
-	size_t cmdsize;
+	char *cmd = NULL;
+	size_t cmdsize = 0;
+	char *args[2];
+	
+	(void)ac;
+	(void)av;
+	(void)en;
 
 	while (1)
 	{
 		printf("OurShell $ " );
 		if (getline(&cmd, &cmdsize, stdin) == -1)
-        {
-           printf("\n");
-            return (0);
-        }
+		{
+			printf("\n");
+			free(cmd);
+			return (0);
+		}
 		cmd[strcspn(cmd, "\n")] = '\0';
-		printf("%s \n" , cmd);
+		args[0] = cmd;
+		args[1] = NULL;
+		if (execve(cmd, args, NULL) == -1)
+		{
+			printf("%s: No such file or directory\n", av[0]);
+		}
+		else
+		{
+			continue;
+		}
 	}
 	return (0);
 }
